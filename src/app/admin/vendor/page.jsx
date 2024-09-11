708110444fd38c7b01eec1336e24ff6b41b4adc0
faizbyp <faizbyp@gmail.com>
2024-09-11 12:14:53 +0700

    feat: add vendor

--- src/app/admin/vendor/page.jsx
+++ src/app/admin/vendor/page.jsx
@@ -12,18 +12,77 @@ import {
   Paper,
   Chip,
   IconButton,
+  Box,
+  Button,
 } from "@mui/material";
+import AddIcon from "@mui/icons-material/Add";
 import InfoIcon from "@mui/icons-material/Info";
 import useFetch from "@/hooks/useFetch";
+import Link from "next/link";
+import { useForm } from "react-hook-form";
+import DialogComp from "@/components/Dialog";
+import { useState } from "react";
+import TextFieldCtrl from "@/components/forms/TextField";
+import CheckboxCtrl from "@/components/forms/Checkbox";
+import API from "@/services/api";
+import toast from "react-hot-toast";
+import { isAxiosError } from "axios";
 
 function VendorPage() {
-  const { data: vendors } = useFetch("/vendor");
+  const { data: vendors, refetch } = useFetch("/vendor");
+  const [openForm, setOpenForm] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const { control, handleSubmit, reset } = useForm({
+    defaultValues: {
+      vendor_name: "",
+      vendor_addr: "",
+      is_active: true,
+    },
+  });
+
+  const handleOpenForm = () => {
+    setOpenForm(true);
+  };
+
+  const handleCloseForm = (event, reason) => {
+    if (reason && reason === "backdropClick") return;
+    reset();
+    setOpenForm(false);
+  };
+
+  const addVendor = async (values) => {
+    setLoading(true);
+    console.log(values);
+
+    try {
+      const res = await API.post("/vendor", values);
+      toast.success(`${res.data.message}:
+        ${res.data.data}`);
+      refetch();
+      handleCloseForm();
+    } catch (error) {
+      if (isAxiosError(error)) {
+        const data = error.response?.data;
+        toast.error(data.message);
+      } else {
+        toast.error("Error");
+      }
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+  };
 
   return (
     <>
-      <Typography variant="h1" sx={{ color: "primary.main" }}>
-        Vendors
-      </Typography>
+      <Box sx={{ mb: 1, display: "flex", gap: 2, alignItems: "center" }}>
+        <Typography variant="h1" sx={{ m: 0, color: "primary.main" }}>
+          Vendors
+        </Typography>
+        <Button variant="outlined" startIcon={<AddIcon />} onClick={handleOpenForm}>
+          Add Vendor
+        </Button>
+      </Box>
       {vendors ? (
         <TableContainer component={Paper} sx={{ maxHeight: 440 }}>
           <Table stickyHeader aria-label="PO Table">
@@ -55,9 +114,11 @@ function VendorPage() {
                     />
                   </TableCell>
                   <TableCell align="right">
-                    <IconButton>
-                      <InfoIcon />
-                    </IconButton>
+                    <Link href={`/admin/vendor/${row.id_vendor}`} passHref>
+                      <IconButton>
+                        <InfoIcon />
+                      </IconButton>
+                    </Link>
                   </TableCell>
                 </TableRow>
               ))}
@@ -67,6 +128,38 @@ function VendorPage() {
       ) : (
         <Skeleton variant="rounded" width="100%" height={64} />
       )}
+
+      <DialogComp
+        title="Add Vendor"
+        open={openForm}
+        onClose={handleCloseForm}
+        actions={
+          <>
+            <Button onClick={handleCloseForm}>Cancel</Button>
+            <Button variant="contained" onClick={handleSubmit(addVendor)} disabled={loading}>
+              Add
+            </Button>
+          </>
+        }
+      >
+        <TextFieldCtrl
+          name="vendor_name"
+          control={control}
+          label="Vendor Name"
+          rules={{
+            required: "Field required",
+          }}
+        />
+        <TextFieldCtrl
+          name="vendor_addr"
+          control={control}
+          label="Address"
+          rules={{
+            required: "Field required",
+          }}
+        />
+        <CheckboxCtrl name="is_active" control={control} label="Active" noMargin />
+      </DialogComp>
     </>
   );
 }
