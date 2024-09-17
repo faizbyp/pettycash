46cb319fb9d1e63c5b5b7558ce62e8b85bdd44a0
faizbyp <faizbyp@gmail.com>
2024-09-17 15:44:04 +0700

    feat: new gr page

---
+++ src/app/dashboard/gr/new/[idPO]/page.jsx
@@ -0,0 +1,15 @@
+import NewGR from "@/components/pages/NewGR";
+
+export async function generateMetadata({ params }) {
+  const idPO = decodeURIComponent(params.idPO);
+
+  return {
+    title: `GR ${idPO} - Petty Cash KPN`,
+  };
+}
+
+export default function NewGRPage({ params }) {
+  const idPO = decodeURIComponent(params.idPO);
+
+  return <NewGR idPO={idPO} />;
+}
