8dba0bc126ea8673ce2aaa0612f10078cc53303b
faizbyp <faizbyp@gmail.com>
2024-09-23 09:22:24 +0700

    fix: add upstream company id

--- src/components/pages/Company.jsx
+++ src/components/pages/Company.jsx
@@ -28,24 +28,32 @@ import toast from "react-hot-toast";
 import { isAxiosError } from "axios";
 import NumericFieldCtrl from "@/components/forms/NumericField";
 import { TableSkeleton } from "../Skeleton";
+import { getValueToPositionMapper } from "@mui/x-charts";
 
 const Company = () => {
   const { data: companies } = useFetch("/company?type=group");
-  const { data: DWCompanies, refetch } = useFetch("/company?type=sub&group=DW");
+  const { data: DWCompanies, refetch: DWRefetch } = useFetch("/company?type=sub&group=DW");
+  const { data: UPCompanies, refetch: UPRefetch } = useFetch("/company?type=sub&group=UP");
   const [openForm, setOpenForm] = useState(false);
   const [loading, setLoading] = useState(false);
-  const { control, handleSubmit, reset } = useForm({
+  const { control, handleSubmit, reset, getValues, setValue } = useForm({
     defaultValues: {
       company_name: "",
       company_addr: "",
       company_phone: "",
       company_fax: "",
       company_type: "sub",
-      company_group: "DW",
+      company_group: "",
     },
   });
 
-  const handleOpenForm = () => {
+  const handleOpenDWForm = () => {
+    setValue("company_group", "DW");
+    setOpenForm(true);
+  };
+
+  const handleOpenUPForm = () => {
+    setValue("company_group", "UP");
     setOpenForm(true);
   };
 
@@ -60,10 +68,11 @@ const Company = () => {
     console.log(values);
 
     try {
-      const res = await API.post("/company", values);
+      const res = await API.post("/company", getValues());
       toast.success(`${res.data.message}:
         ${res.data.data}`);
-      refetch();
+      DWRefetch();
+      UPRefetch();
       handleCloseForm();
     } catch (error) {
       if (isAxiosError(error)) {
@@ -120,13 +129,13 @@ const Company = () => {
         )}
       </Box>
 
-      <Box component="section">
+      <Box component="section" sx={{ mb: 6 }}>
         <Box sx={{ mb: 1, display: "flex", gap: 2, alignItems: "center" }}>
           <Typography variant="h2" sx={{ m: 0, color: "primary.main" }}>
             Downstream Companies
           </Typography>
-          <Button variant="outlined" startIcon={<AddIcon />} onClick={handleOpenForm}>
-            Add Company
+          <Button variant="outlined" startIcon={<AddIcon />} onClick={handleOpenDWForm}>
+            Add DW Company
           </Button>
         </Box>
         {DWCompanies ? (
@@ -173,8 +182,61 @@ const Company = () => {
         )}
       </Box>
 
+      <Box component="section" sx={{ mb: 6 }}>
+        <Box sx={{ mb: 1, display: "flex", gap: 2, alignItems: "center" }}>
+          <Typography variant="h2" sx={{ m: 0, color: "primary.main" }}>
+            Upstream Companies
+          </Typography>
+          <Button variant="outlined" startIcon={<AddIcon />} onClick={handleOpenUPForm}>
+            Add UP Company
+          </Button>
+        </Box>
+        {UPCompanies ? (
+          <TableContainer component={Paper} sx={{ maxHeight: 440 }}>
+            <Table stickyHeader aria-label="PO Table">
+              <TableHead>
+                <TableRow>
+                  <TableCell>ID</TableCell>
+                  <TableCell>Company Name</TableCell>
+                  <TableCell>Address</TableCell>
+                  <TableCell>Phone</TableCell>
+                  <TableCell>Fax</TableCell>
+                  <TableCell align="right">Action</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {UPCompanies.data.map((row) => (
+                  <TableRow
+                    hover
+                    key={row.id}
+                    sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                  >
+                    <TableCell component="th" scope="row">
+                      {row.id_company}
+                    </TableCell>
+                    <TableCell>{row.company_name}</TableCell>
+                    <TableCell>{row.company_addr}</TableCell>
+                    <TableCell>{row.company_phone}</TableCell>
+                    <TableCell>{row.company_fax}</TableCell>
+                    <TableCell align="right">
+                      <Link href={`/admin/company/${row.id_company}`} passHref>
+                        <IconButton>
+                          <InfoIcon />
+                        </IconButton>
+                      </Link>
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        ) : (
+          <TableSkeleton column={6} />
+        )}
+      </Box>
+
       <DialogComp
-        title="Add Company"
+        title={`Add Company (${getValues("company_group")})`}
         open={openForm}
         onClose={handleCloseForm}
         actions={
