8c9a75a3a764fb7ca9e49a4346beaca5ec71cde2
faizbyp <faizbyp@gmail.com>
2024-10-18 09:33:14 +0700

    fix: font load on next js

--- src/app/main-layout.jsx
+++ src/app/main-layout.jsx
@@ -10,7 +10,7 @@ import { Roboto } from "next/font/google";
 const roboto = Roboto({
   weight: ["100", "300", "400", "500", "700", "900"],
   subsets: ["latin"],
-  display: "swap",
+  display: "fallback",
 });
 
 export const metadata = {
